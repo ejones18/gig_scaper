eaa22c7df5bb872d5dfea19ca63ebb4a90088d46
= <ejones18@sheffield.ac.uk>
2020-09-03 12:21:08 +0100

    Increased functionality for flask app

--- src/gig_flask.py
+++ src/gig_flask.py
@@ -8,7 +8,7 @@ from flask import Flask, request, redirect, render_template
 
 import gig_scanner
 
-#gig_scanner.main("Birmingham")
+
 
 APP = Flask(__name__)
 
@@ -18,6 +18,17 @@ def home_page():
     Home page of the flask app.
     """
     return render_template("home_page.html")
+    
+@APP.route("/gig_results", methods=["GET", "POST"])
+def gig_results():
+    """Scans for gigs used passed in file."""
+    if request.method == "POST" and "artistFileUpload" in request.files:
+        artists_file = request.files["artistFileUpload"].stream
+        gigs = gig_scanner.main(artists_file, False)
+        html_table = gigs.to_html()
+        return render_template("gig_results.html", table=html_table)
+    else:
+        return redirect("home_page.html", message="Error")
 
 if __name__ == "__main__":
     APP.run()
